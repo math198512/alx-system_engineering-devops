c60c835dc512c96c8e67f784a05c7c0fa8cb2392
math198512 <thoraichi@gmail.com>
2024-04-14 15:00:49 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ returns the number of subscribers
 If an invalid subreddit is given,
 the function should return 0.
 """
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
@@ -13,19 +13,20 @@ def number_of_subscribers(subreddit):
     """
     # Set the Default URL strings
     base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
+    api_uri = '{}/r/{}/about.json'.format(base=base_url, subreddit=subreddit)
 
     # Set an User-Agent
     user_agent = {'User-Agent': 'Python/requests'}
 
     # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
+    res = get(api_uri, headers=user_agent,
                        allow_redirects=False)
-
+    
     # Checks if the subreddit is invalid
     if res.status_code in [302, 404]:
         return 0
 
     # Returns the total subscribers of the subreddit
-    return res.json().get('data').get('subscribers')
+    data = res.json()
+    subscribers = data['data']['subscribers']
+    return subscribers
