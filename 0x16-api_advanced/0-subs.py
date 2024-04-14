12da8842e9eba1168ea075f57053aa967877f736
math198512 <thoraichi@gmail.com>
2024-04-14 15:06:45 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and
-returns the number of subscribers
-If an invalid subreddit is given,
-the function should return 0.
 """
-from requests import get
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a given subreddit.
-    """
-    # Set the Default URL strings
-    base_url = 'https://www.reddit.com'
-    api_uri = '{}/r/{}/about.json'.format(base=base_url, subreddit=subreddit)
-
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
-
-    # Get the Response of the Reddit API
-    res = get(api_uri, headers=user_agent,
-                       allow_redirects=False)
-    
-    # Checks if the subreddit is invalid
-    if res.status_code in [302, 404]:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    # Returns the total subscribers of the subreddit
-    data = res.json()
-    subscribers = data['data']['subscribers']
-    return subscribers
