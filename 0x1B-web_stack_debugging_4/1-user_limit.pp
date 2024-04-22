0632d97cf0ac81756ce00dad7cd27b3865f6f3f9
math198512 <thoraichi@gmail.com>
2024-04-22 13:45:05 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+#Enable the user holberton to login and open files without errors
+
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin/'
