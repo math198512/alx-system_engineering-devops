9ee8e970283405d6e11a716da85633869e096466
math198512 <thoraichi@gmail.com>
2024-06-06 12:37:00 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,22 @@
 """function that queries the Reddit API and returns the number of subscribers
 for a given subreddit"""
 
-import requests
+import requests, requests.auth
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    """Return the total number of subscribers on a given subreddit."""
+    username = "tarikmath512"
+    password = "8ig8W7Z$e$Zev*z"
+    CLIENT_ID = "VJEVTB5HwkOSJq8wSdrucg"
+    CLIENT_SECRET = "JA0DqIER3KiHc39Oy19cJ43DRsJOTw"
+    client_auth = ""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data').get('subscribers')
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
