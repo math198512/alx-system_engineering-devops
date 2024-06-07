5a021facd8b8ce37678a8339a9b6db3d10cf8cbd
math198512 <thoraichi@gmail.com>
2024-06-07 01:15:26 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,8 @@
 """function that queries the Reddit API and returns the number of subscribers
 for a given subreddit"""
 
-import requests, requests.auth
+import requests
+import requests.auth
 
 
 def number_of_subscribers(subreddit):
@@ -11,12 +12,17 @@ def number_of_subscribers(subreddit):
     password = "8ig8W7Z$e$Zev*z"
     CLIENT_ID = "VJEVTB5HwkOSJq8wSdrucg"
     CLIENT_SECRET = "JA0DqIER3KiHc39Oy19cJ43DRsJOTw"
-    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-    post_data = {"grant_type": "password", "username": username, "password": password}
+    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID,
+                                              CLIENT_SECRET)
+    post_data = {"grant_type": "password",
+                 "username": username,
+                 "password": password}
     TOKEN_ACCESS_ENDPOINT = "https://www.reddit.com/api/v1/access_token"
-    
     headers = {"User-Agent": "Mozilla/5.0"}
-    auth_response = requests.post(TOKEN_ACCESS_ENDPOINT, data=post_data, headers=headers, auth=client_auth)
+    auth_response = requests.post(TOKEN_ACCESS_ENDPOINT,
+                                  data=post_data,
+                                  headers=headers,
+                                  auth=client_auth)
     TOKEN = auth_response.json()['access_token']
     headers['Authorization'] = f'bearer {TOKEN}'
     url = "https://oauth.reddit.com/r/{}/about/".format(subreddit)
